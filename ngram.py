eea72e8932908c7f58729f5616a8bf9fb2feeef7
PA <45755573+EquinetPaul@users.noreply.github.com>
2023-04-13 16:57:01 +0200

    Update ngram.py

--- ngram.py
+++ ngram.py
@@ -7,18 +7,44 @@ class Ngram():
     def train(self, text, separator = " "):
         words = text.split(separator)
         for i in range(len(words)-self.n):
-            get = self.chain_frequency.get(tuple([words[i+y] for y in range(self.n)]),0)
-            if get == 0:
-                self.chain_frequency[tuple([words[i+y] for y in range(self.n)])] = {}
-                self.chain_frequency[tuple([words[i+y] for y in range(self.n)])][words[i+self.n]] = 1
-            elif get.get(words[i+self.n],0) == 0:
-                self.chain_frequency[tuple([words[i+y] for y in range(self.n)])][words[i+self.n]] = 1
+            if self.n>1:
+                sequence = tuple([words[i+y] for y in range(self.n)])
             else:
-                self.chain_frequency[tuple([words[i+y] for y in range(self.n)])][words[i+self.n]] += 1
+                sequence = words[i]
+            get = self.chain_frequency.get(sequence,0)
+            if get == 0:
+                self.chain_frequency[sequence] = {}
+                self.chain_frequency[sequence][words[i+self.n]] = 1
+            elif get.get(words[i+self.n],0) == 0:
+                self.chain_frequency[sequence][words[i+self.n]] = 1
+            else:
+                self.chain_frequency[sequence][words[i+self.n]] += 1
                 
     def normalize(self):
-        self.chain = self.chain_frequency.copy()
-        for key, value in self.chain_frequency.items():
+        self.chain = copy.deepcopy(self.chain_frequency)
+        for key, value in self.chain.items():
             sum_frequency = sum(value.values())
             for k, v in value.items():
-                self.chain[key][k] = v / sum_frequency
+                self.chain[key][k] = v / sum_frequency    
+                
+    def generate_sentence(self):
+        sentence = "Mesdames et Messieurs"
+        while sentence[-1] not in [".", "!", "?"]:
+            if self.n>1:
+                current_sequence = tuple(sentence.split()[-self.n:])
+            else:
+                current_sequence = sentence.split()[-1]
+            sentence += " " + random.choices(list(self.chain[current_sequence].keys()), weights=list(self.chain[current_sequence].values()))[0]
+        sentence = " ".join(sentence.split())
+        return sentence
+    
+    def save(self, filepath):
+        with open(filepath, 'wb') as save_file:
+            pickle.dump(self.__dict__, save_file)
+            
+    def load(self, filepath):
+        with open(filepath, 'rb') as load_file:
+            obj = pickle.load(load_file)
+            self.n = obj["n"]
+            self.chain_frequency = obj["chain_frequency"]
+            self.chain = obj["chain"]
